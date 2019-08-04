4260a770643e66ff189e3da18af41b0c2e525d7f
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-08-05 01:38:27 +0530

    Global Exception using @ControllerAdvice is done.

--- src/main/java/com/stackroute/trackservice/exceptions/GlobalResponseEntityExceptionHandler.java
+++ src/main/java/com/stackroute/trackservice/exceptions/GlobalResponseEntityExceptionHandler.java
@@ -2,11 +2,12 @@ package com.stackroute.trackservice.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+@ControllerAdvice
 public class GlobalResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value= { IllegalArgumentException.class, IllegalStateException.class })
+    @ExceptionHandler(value= { TrackAlreadyExistsException.class, TrackNotFoundException.class })
     protected ResponseEntity<Object> handleConflict(Exception ex) {
 
         return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
