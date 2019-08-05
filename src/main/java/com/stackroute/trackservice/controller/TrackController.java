c27b751cda04669b546e505a0f8ca98db70cacfb
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-08-05 12:36:04 +0530

    different properties for h2 and mysql

--- src/main/java/com/stackroute/trackservice/controller/TrackController.java
+++ src/main/java/com/stackroute/trackservice/controller/TrackController.java
@@ -6,6 +6,7 @@ import com.stackroute.trackservice.exceptions.TrackNotFoundException;
 import com.stackroute.trackservice.service.TrackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
